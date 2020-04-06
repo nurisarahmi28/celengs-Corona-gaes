14f5ea63b8b07f9bc57407b851a1083e1eb636be
Risa <54298936+nurisarahmi28@users.noreply.github.com>
2020-04-06 20:52:05 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,4 @@
+<script src="http://localhost/nilai_siswa/asset/jquery.js"></script>
+<script src="http://localhost/nilai_siswa/asset/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<style>
+.container{
+    position: relative;
+}
+    .bottomright {
+        position: absolute;
+        bottom: 16px;
+        right: 16px;
+    }
+   img{
+       width: 42px;
+       height: 42px;
+   }
+</style>
+<head>
+<title>nilai siswa</title>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible"content="ie=edge">
+
+    <link rel="stylesheet" href="http://localhost/nilai_siswa/asset/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost/nilai_siswa/asset/fontawesome/css/all.min.css">
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #ea6227;">
+<a class="navbar-brand"href="http://localhost/nilai_siswa/student/index.php"><i class="fas fa-star-half-alt"></i>Students Data</a>
+   <button class="navbar-toggler"type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup">
+     <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-item nav-link" href="http://localhost/nilai_siswa/student/index.php">Students Score</a>
+            <a class="nav-item nav-link" href="http://localhost/nilai_siswa/student/tambah.php">Add Data</a>
+            <a target="_blank" class="nav-item nav-link" href="https://youtu.be/-WO7iY2x7dk">Logout</a>
+    </div>
+        </div>
+</div>
+</nav>
+
+  <script src="http://localhost/nilai_siswa/asset/jquery.js"></script>
+  <script src="http://localhost/nilai_siswa/asset/bootstrap/js/bootstrap.min.js"></script>
+  <a href="https://www.instagram.com/risaaaarh/" target="_blank">
+   <img class="bottomright" src="https://image.flaticon.com/icons/svg/2111/2111463.svg" alt="Instagram">
+   </a>
+</body>
+</html>
